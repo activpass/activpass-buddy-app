6e3091cefb81df4ad7e01875d6efd3e07083652f
paalamugan-work <paalamugan26@gmail.com>
2025-09-01 19:40:27 +0530

    refactor: instantiate imagekit within deletefilefromimagekit function

--- src/server/api/utils/imagekit.ts
+++ src/server/api/utils/imagekit.ts
@@ -1,15 +1,16 @@
+'use server';
+
 import ImageKit from 'imagekit';
 
 import { env } from '@/env';
 
-export const imageKit = new ImageKit({
-  privateKey: env.IMAGEKIT_PRIVATE_KEY,
-  publicKey: env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY,
-  urlEndpoint: env.NEXT_PUBLIC_IMAGEKIT_URL_ENDPOINT,
-});
-
 export const deleteFileFromImageKit = async (fileId: string) => {
   try {
+    const imageKit = new ImageKit({
+      privateKey: env.IMAGEKIT_PRIVATE_KEY,
+      publicKey: env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY,
+      urlEndpoint: env.NEXT_PUBLIC_IMAGEKIT_URL_ENDPOINT,
+    });
     await imageKit.deleteFile(fileId);
   } catch (error) {
     const err = error as Error;
